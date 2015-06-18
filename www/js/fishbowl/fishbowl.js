4cbba9a06c272ff7c44852a8b164bb976d19cf23
Mark Hall <mankindsoftware@gmail.com>
2015-06-18 10:45:31 -0700

    first commit. no code cleanup yet.

---
+++ www/js/fishbowl/fishbowl.js
@@ -0,0 +1,55 @@
+angular.module('fishbowl', [])
+
+.directive('transition', function() {
+    return {
+        restrict: 'AC',
+        controller: 'transitionController',
+        link: function(scope, element, attrs) {
+
+        }
+    };
+})
+.controller('transitionController', ['$scope', '$ionicModal', 'fsmService', 'waterlineServiceProvider', function($scope, $ionicModal, fsmService, waterlineServiceProvider) {
+	$scope.fsm = null;
+	$scope.ws = null;
+	var level = null;
+
+    $ionicModal.fromTemplateUrl('js/fishbowl/fishbowl.html', {
+        scope: $scope,
+        animation: 'slide-in-up',
+        fsmName: '',
+    })
+    .then(function(modal) {
+        $scope.modal = modal;
+    });
+
+    $scope.openModal = function(namespace) {
+
+        var fsm = fsmService[namespace];
+        var ws = fsm.ws;
+
+        fsm.on('newQty', function(data) {
+        	ws.setQty(data.qty);
+        })
+
+        fsm.on('goal', function(data) {
+        	ws.setOpt('goal', data.goal);
+        })
+
+        $scope.transition = function(state) {
+            $scope.modal.hide();
+	        fsm.transition(state);
+        };
+
+        $scope.modal.show();
+    };
+
+    $scope.closeModal = function() {
+        $scope.modal.hide();
+    };
+
+    $scope.$on('$destroy', function() {
+        $scope.modal.remove();
+    });
+
+}]);
---
+++ www/readme.txt
@@ -0,0 +1,11 @@
+Solutions Fill the 3-liter bottle and pour it into the empty 5-liter bottle.
+Fill the 3-liter bottle again, and pour enough to fill 5-liter bottle. This leaves exactly 1 liter in the 3-liter bottle.
+Empty the 5-liter bottle; pour the remaining 1 liter from the 3-liter bottle into the 5-liter bottle.
+Fill the 3-liter bottle and pour it into the 5-liter bottle. The 5-liter bottle now has exactly 4 liters.
+
+Here is another way to do it*
+Fill the 5-liter bottle and pour water from it into the 3-liter bottle until it is full.
+This leaves 2 liters in the 5-liter bottle.
+Empty the 3-liter bottle and pour the 2 liters of water from the 5-liter bottle into the 3-liter bottle.
+Fill the 5-liter bottle again.
+Fill the 3-liter bottle from the 5-liter bottle. Since the 3-liter bottle had 2 liters of water, only one liter is transferred leaving exactly 4 liters of water in the 5-liter jug.
